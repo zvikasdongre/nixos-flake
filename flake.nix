724350f7227dcef1ee969fd3c6afe5343befb44e
Vikas Dongre <zvikasdongre@gmail.com>
2025-11-23 15:52:18 +0530

    init: initial working flake (gen-64)

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "NixOS flake for my laptop";
+
+  inputs = {
+    #nixpkgs.url = "github:nixos/nixpkgs?ref=25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    
+    # Wallpaper manager
+    swww = {
+      url = "github:LGFae/swww";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # Launcher
+    vicinae.url = "github:vicinaehq/vicinae";
+  };
+
+  nixConfig = {
+    extra-substituters = [ "https://vicinae.cachix.org" ];
+    extra-trusted-public-keys = [ "vicinae.cachix.org-1:1kDrfienkGHPYbkpNj1mWTr7Fm1+zcenzgTizIcI3oc=" ];
+  };
+  
+  outputs = { self, nixpkgs, ... } @ inputs: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      specialArgs = { inherit inputs; };
+      modules = [ ./nixos/configuration.nix ];
+    };
+  };
+}
---
+++ nixos/configuration.nix
@@ -0,0 +1,302 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{
+  config,
+  pkgs,
+  inputs,
+  lib,
+  ...
+}:
+
+{
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+  ];
+
+  # Use the GRUB 2 boot loader.
+  boot.loader = {
+    timeout = 10;
+
+    efi = {
+      efiSysMountPoint = "/boot/efi";
+      canTouchEfiVariables = true;
+    };
+
+    grub = {
+      enable = true;
+      efiSupport = true;
+      efiInstallAsRemovable = false; # Otherwise /boot/EFI/BOOT/BOOTX64.EFI isn't generated
+      devices = [ "nodev" ];
+      useOSProber = true;
+      extraEntriesBeforeNixOS = true;
+    };
+  };
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Enable bluetooth
+  hardware.bluetooth.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Asia/Kolkata";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_IN";
+    LC_IDENTIFICATION = "en_IN";
+    LC_MEASUREMENT = "en_IN";
+    LC_MONETARY = "en_IN";
+    LC_NAME = "en_IN";
+    LC_NUMERIC = "en_IN";
+    LC_PAPER = "en_IN";
+    LC_TELEPHONE = "en_IN";
+    LC_TIME = "en_IN";
+  };
+
+  # Enable the X11 windowing system.
+  # You can disable this if you're only using the Wayland session.
+  services.xserver.enable = true;
+
+  # Enable SDDM.
+  services.displayManager.sddm = {
+    enable = true;
+    wayland.enable = true;
+    enableHidpi = true;
+
+    theme = "${
+      pkgs.sddm-astronaut.override {
+        embeddedTheme = "japanese_aesthetic";
+      }
+    }/share/sddm/themes/sddm-astronaut-theme";
+
+    settings = {
+      General = {
+        GreeterEnvironment = lib.concatStringsSep "," [
+          "QT_QPA_PLATFORM=wayland"
+          "QT_WAYLAND_FORCE_DPI=192"
+          "QT_SCALE_FACTOR=1"
+          "QT_AUTO_SCREEN_SCALE_FACTOR=0"
+          "QT_SCREEN_SCALE_FACTORS=2"
+          "QT_FONT_DPI=192"
+        ];
+      };
+
+      Theme = {
+        CursorTheme = "Bibata-Modern-Classic";
+        CursorSize = 24;
+      };
+    };
+  };
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  services.gnome.gnome-keyring.enable = true;
+
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+  security.pam.services.sddm.enableGnomeKeyring = true;
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.vikas = {
+    isNormalUser = true;
+    description = "Vikas Dongre";
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+    ];
+    packages = with pkgs; [
+      kdePackages.kate
+      #  thunderbird
+    ];
+  };
+
+  # Install firefox.
+  programs.firefox.enable = true;
+
+  # Hyprland
+  programs.hyprland = {
+    enable = true;
+    withUWSM = true;
+  };
+
+  # Setup git
+  programs.git = {
+    enable = true;
+    config = {
+      user.name = "Vikas Dongre";
+      user.email = "zvikasdongre@gmail.com";
+      init.defaultBranch = "main";
+    };
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    # Essentials / Utilities
+    wget
+    jq
+    gnome-keyring
+    libsecret
+    brightnessctl
+    dunst
+    libnotify
+    xdg-desktop-portal
+    hyprpolkitagent
+    xstow
+    efibootmgr
+    nixfmt-rfc-style
+
+    # Fonts
+    jetbrains-mono
+    ubuntu-sans
+    noto-fonts
+
+    # Common Programs
+    vscode
+    neovim
+    nemo-with-extensions
+    fastfetch
+    ghostty
+    gh
+    cava
+    starship
+    stow
+    atuin
+    btop
+
+    # launcher
+    inputs.vicinae.packages.${pkgs.system}.default
+
+    # screenshot utilities
+    grimblast
+    grim
+    slurp
+    hyprpicker
+    wl-clipboard
+
+    # swww
+    inputs.swww.packages.${pkgs.system}.swww
+
+    # quickshell
+    quickshell
+    kdePackages.qtsvg
+    kdePackages.qt5compat
+    kdePackages.qtimageformats
+    kdePackages.qtmultimedia
+
+    # themes & theming utilities
+    mint-cursor-themes
+    afterglow-cursors-recolored
+    kvmarwaita
+    whitesur-icon-theme
+    whitesur-gtk-theme
+    nwg-look
+    libsForQt5.qt5ct
+    kdePackages.qt6ct
+    kdePackages.qtstyleplugin-kvantum
+    sddm-astronaut
+  ];
+
+  fonts = {
+    packages = with pkgs; [
+      jetbrains-mono
+      ubuntu-sans
+      noto-fonts
+    ];
+    fontconfig = {
+      defaultFonts = {
+        serif = [
+          "Liberation Serif"
+          "Vazirmatn"
+        ];
+        sansSerif = [
+          "Ubuntu Sans"
+          "Vazirmatn"
+        ];
+        monospace = [ "JetBrains Mono" ];
+      };
+    };
+    fontDir.enable = true;
+  };
+
+  qt = {
+    enable = true;
+    platformTheme = "qt5ct";
+    style = "kvantum";
+  };
+
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+  environment.variables.XDG_RUNTIME_DIR = "/run/user/$UID";
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.05"; # Did you read the comment?
+
+}
